85b3da60f08f419cc306af987cb673bceada3ed6
marvinlin <marvinlink07@gmail.com>
2020-03-18 19:26:32 -0600

    test

--- src/pages/BudgetNew.js
+++ src/pages/BudgetNew.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Redirect } from "react-router-dom";
 export default class BudgetNew extends Component {
 
     constructor() {
