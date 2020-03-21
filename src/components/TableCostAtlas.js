b6d462f35ee21479ac0de617ffe25b43fb1cbb77
marvinlin <marvinlink07@gmail.com>
2020-03-21 00:32:30 -0600

    test

--- src/components/TableCostAtlas.js
+++ src/components/TableCostAtlas.js
@@ -230,7 +230,7 @@ import { Redirect} from 'react-router-dom';
 
         //codigo para crear un nuevo renglon presupuestario
     onSubmit  = async e =>{
-        e.preventDefault();
+        //e.preventDefault();
         const res = await axios.post('https://backendihcafe.herokuapp.com/api/budgetlines/budgetlineatlas',{
             code_resultado:this.state.result_atlas,
             code_producto:this.state.product_atlas,
@@ -278,12 +278,12 @@ import { Redirect} from 'react-router-dom';
 
     //funcion para elimiar un renglon presupuestario
     onSumbitDelete  = async (e,id) =>{
-        e.preventDefault();
+        //e.preventDefault();
         const res_p = await axios.post('https://backendihcafe.herokuapp.com/api/budgetlines/budgetlineatlas/delete/'+id);
         
         if (res_p) {
-             //return <Redirect to={"/project/"+this.props.idProject}  />
-             return <Redirect to={"/budgets"}  />
+             return <Redirect to={"/project/"+this.props.idProject}  />
+             //return <Redirect to={"/budgets"}  />
 
         }
        
