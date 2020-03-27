8bf0143dfb1631cb24abc0b3803d0aaf0452f65f
marvinlin <marvinlink07@gmail.com>
2020-03-27 00:47:37 -0600

    test

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-<div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.24.0/babel.js"></script>
-<script type='text/babel'>
+
 ReactDOM.render(<App />, document.getElementById('root'));
-</script>
-</div>
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
