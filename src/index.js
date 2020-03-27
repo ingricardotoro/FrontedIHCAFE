6acf0e7d330cd4dda987b9e1842ee08b1c523d00
marvinlin <marvinlink07@gmail.com>
2020-03-26 23:18:16 -0600

    add babel

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 
 <script type='text/babel'>
+const JSX = (
+    <div>
+      <h1>Hello World</h1>
+      <p>Lets render this to the DOM</p>
+    </div>
+  );
 ReactDOM.render(<App />, document.getElementById('root'));
 </script>
 
