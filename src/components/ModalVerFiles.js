bec9c201165182bb972daa95c8ea626e7edf7c5e
marvinlin <marvinlink07@gmail.com>
2020-03-28 21:01:15 -0600

    test

--- src/components/ModalVerFiles.js
+++ src/components/ModalVerFiles.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Redirect } from 'react-router-dom';
 export default class ModalVerFiles extends Component {
 
     constructor() {
