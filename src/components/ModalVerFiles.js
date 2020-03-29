6ca97f6676fa612be3b215b248efc3708ba3f1cc
marvinlin <marvinlink07@gmail.com>
2020-03-29 14:07:15 -0600

    this

--- src/components/ModalVerFiles.js
+++ src/components/ModalVerFiles.js
@@ -69,7 +69,7 @@ export default class ModalVerFiles extends Component {
                                                                     
                                                                     <div className="mt-3 red">
                                                                        
-                                                                        <button type="button" onClick={ () => onClickDelete(file.id)} className="btn btn-sm btn-danger" >Eliminar Archivo </button>
+                                                                        <button type="button" onClick={ () => this.onClickDelete(file.id)} className="btn btn-sm btn-danger" >Eliminar Archivo </button>
                                                                        
                                                                     </div>
                                                             </div>
