f28d01f78c56c30aa6a47888688833833184a6cd
Marvin Toro <marvinlink07@gmail.com>
2022-01-13 23:03:08 -0600

    others changes test

---
+++ build/precache-manifest.fb2c40c17a81ab20bdb04a793c2e3c4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599f7f3c04f33dea1ce2a0777ee76144",
+    "url": "/index.html"
+  },
+  {
+    "revision": "818bf8a9f1ee79128236",
+    "url": "/static/js/2.a718f023.chunk.js"
+  },
+  {
+    "revision": "710d5a8fd3b1b9c5771de41b433a67d0",
+    "url": "/static/js/2.a718f023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f22a273a7c65efa83b39",
+    "url": "/static/js/main.32c9c408.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
---
+++ src/routes/routes.js
@@ -0,0 +1,64 @@
+/* import React from "react";
+import { Navigate } from "react-router-dom";
+
+import Sidebar from "../components/Sidebar";
+
+import Atlas from "../pages/Atlas";
+import AtlasAccount from "../pages/AtlasAccount";
+
+import listBudgets from "../components/listBudgets";
+import Navbar from "../components/Navbar";
+import projectDashboard from "../pages/ProjectDashboard";
+import BudgetNew from "../pages/BudgetNew";
+import BudgetEdit from "../pages/BudgetEdit";
+import ListProjects from "../pages/ListProjects";
+import ProjectNew from "../pages/ProjectNew";
+import ProjectEdit from "../pages/ProjectEdit";
+
+import ListCategories from "../pages/ListCategories";
+import BudgetLineNew from "../pages/BudgetLineNew";
+import BudgetLineEdit from "../pages/BudgetLineEdit";
+
+import pageSupplierslist from "../pages/Suppliers_list";
+import Users_list from "../pages/Users_list";
+import Teams_list from "../pages/Teams_list";
+import Teams_New from './pages/TeamNew';
+import TeamMembers from './pages/TeamMembers';
+//import Roles from './pages/Roles'
+
+import Account_list from "../pages/Accounts";
+import Account_new from "../pages/AccountNew";
+import AccountDashboard from "../pages/AccountDashboard";
+
+import ReportsAtlas from './pages/ReportsAtlas'
+import Reports from "../pages/Reports";
+import ReportsUsers from './pages/reports/ReportsUsers'
+import ReportAtlasByProjectid from './pages/reports/ReportAtlasByProjectid'
+import ReportsAtlasBudgetByProAndAtlasAccount from './pages/reports/atlas/budgets_by_projectid_and_atlasaccountid'
+import ReportsAtlasBudgetByResults from './pages/reports/atlas/report_atlas_by_results'
+
+import Coinlist from './pages/Coinlist'
+import CoinNew from './pages/CoinNew'
+import ConversionList from './pages/Conversions'
+
+import Login from './pages/Login'
+import Inicio from './pages/Inicio'
+import Dasboard from './pages/Dasboard'
+
+import { ToastContainer } from 'react-toastify'
+//import 'react-toastify/dist/ReactToastify.css';
+//import 'react-toastify/scss/main.scss'
+
+
+const Router = [
+    {
+        path: '/',
+        element: <Navbar />,
+        element: <Sidebar />,
+        children: [
+            { path: '/', exact: true, element: <Sidebar /> }
+        ]
+    }
+]
+export default Router;
+ */
